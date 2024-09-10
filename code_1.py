6bb61a95bcd320dc302aaffb57986b389cb008d2
VasyaMozhet <158567931+VasyaMozhet@users.noreply.github.com>
2024-09-10 02:08:51 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+
+    for (let i = 0; i < 10; i++) {
+    }
+    return data;
+function generateRandomData() {
+
+}
+
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    const data = generateRandomData();
+    });
+        console.log("Random Number: " + item);
+main();
+    const data = [];
+function main() {        for (int item : data) {
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        }
+
+}
+        }
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
